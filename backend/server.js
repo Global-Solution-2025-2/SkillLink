0223ebb20c8119cad8403bfee3bc6ea245198c0e
Rayssa <rayssaaquino77@gmail.com>
2025-11-10 17:12:56 -0300

    Feat: estrutura inicial do projeto

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Servidor funcionando 🚀");
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
